58343fb85d05a2bce6ae444985932970706efb3d
jonathandick <jonathan.j.dick@gmail.com>
2015-04-01 22:33:54 +0300

    small change

--- flat_tables/flat_int_data.sql
+++ flat_tables/flat_int_data.sql
@@ -7,19 +7,6 @@
 # 3. Add column definitions 
 # 4. Add obs_set column definitions
 
-# first check if in flat_log
-select @last_update := (select max(date_updated) from flat_log where table_name="flat_int_data");
-
-# then use the max_date_created from amrs.encounter
-select @last_update :=
-	if(@last_update is null, 
-		(select max(date_created) from amrs.encounter e join flat_int_data using (encounter_id)),
-		@last_update);
-
-#otherwise set to a date before any encounters had been created (i.g. we will get all encounters)
-select @last_update := if(@last_update,@last_update,'1900-01-01');
-
-select @now := now();
 
 # drop table if exists flat_int_data;
 #delete from flat_log where table_name="flat_int_data";
@@ -88,6 +75,19 @@ int_hiv_vl_quant_date datetime,
 index encounter_id (encounter_id)
 );
 
+# first check if in flat_log
+select @last_update := (select max(date_updated) from flat_log where table_name="flat_int_data");
+
+# then use the max_date_created from amrs.encounter
+select @last_update :=
+	if(@last_update is null, 
+		(select max(date_created) from amrs.encounter e join flat_int_data using (encounter_id)),
+		@last_update);
+
+#otherwise set to a date before any encounters had been created (i.g. we will get all encounters)
+select @last_update := if(@last_update,@last_update,'1900-01-01');
+
+select @now := now();
 
 
 
