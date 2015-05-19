386f639ecc7f933d70701624f8613cf38785f042
jonathandick <jonathan.j.dick@gmail.com>
2015-05-18 20:42:21 -0400

    updated flat_defaulters query

--- calculated_tables/defaulters.sql
+++ calculated_tables/defaulters.sql
@@ -11,8 +11,9 @@ join derived_encounter using (encounter_id)
 where next_encounter_datetime is null #and rtc_date <= date_sub(now(),interval 90 day) 
 	and if(rtc_date,date_add(rtc_date,interval 90 day),date_add(t1.encounter_datetime,interval 180 day)) <= now()
 	and death_date is null
-	and transfer_out is null
+	and out_of_care is null
 );
+
 
 
 drop table if exists flat_defaulters;
@@ -20,9 +21,8 @@ create table flat_defaulters (
 	person_id int, 
 	location_id int, 
 	location_uuid varchar (50), 
-	days_since_rtc int, 
 	primary key (person_id), 
-	index loc_person (location_uuid,days_since_rtc)
+	index loc_rtc (location_uuid,rtc_date)
 )
 (select 
 	t1.person_id, 
@@ -35,7 +35,7 @@ create table flat_defaulters (
 	t4.name as encounter_type_name,
 	concat(t5.given_name,' ',t5.middle_name,' ',t5.family_name) as person_name, 
 	t6.value as phone_number,
-	group_concat(t7.identifier separator ', ') as identifiers,
+	group_concat(distinct t7.identifier separator ', ') as identifiers,
 	t1.uuid as patient_uuid
 
 from (select @start_range_high_risk := 8) p0a,(select @start_range := 30) p0b, (select @end_range := 89) p0c,
@@ -45,15 +45,9 @@ from (select @start_range_high_risk := 8) p0a,(select @start_range := 30) p0b, (
 	join amrs.encounter t3 using (encounter_id)
 	join amrs.encounter_type t4 on t3.encounter_type = t4.encounter_type_id 
 	join amrs.person_name t5 on t1.person_id = t5.person_id and t5.voided=0
-	left outer join amrs.person_attribute t6 on t1.person_id = t6.person_id and t6.person_attribute_type_id=10 and t6.voided=0
 	left outer join amrs.patient_identifier t7 on t1.person_id = t7.patient_id
-
-where 
-	next_encounter_datetime is null 
-	and death_date is null
-	and out_of_care is null
+	left outer join amrs.person_attribute t6 on t1.person_id = t6.person_id and t6.person_attribute_type_id=10 and t6.voided=0
 group by t1.person_id
 );
 
 select concat("Time to complete flat_defaulters table: ",timestampdiff(minute, @start, now())," minutes");
-
