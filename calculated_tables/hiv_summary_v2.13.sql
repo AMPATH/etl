df325614c034de43bb8f79d4e0ab0eab4e10047b
Nicky Kimaina <nkimaina@ampath.or.ke>
2017-09-21 16:30:31 +0300

    Added doltegravir drug

--- calculated_tables/hiv_summary_v2.13.sql
+++ calculated_tables/hiv_summary_v2.13.sql
@@ -560,13 +560,13 @@ DELIMITER $$
 								when obs regexp "!!1255=(1107|1260)!!" then @cur_arv_line := null
 								when obs regexp "!!1250=(6467|6964|792|633|631)!!" then @cur_arv_line := 1
 								when obs regexp "!!1250=(794|635|6160|6159)!!" then @cur_arv_line := 2
-								when obs regexp "!!1250=6156!!" then @cur_arv_line := 3
+								when obs regexp "!!1250=(6156|9759)!!" then @cur_arv_line := 3
 								when obs regexp "!!1088=(6467|6964|792|633|631)!!" then @cur_arv_line := 1
 								when obs regexp "!!1088=(794|635|6160|6159)!!" then @cur_arv_line := 2
-								when obs regexp "!!1088=6156!!" then @cur_arv_line := 3
+								when obs regexp "!!1088=(6156|9759)!!" then @cur_arv_line := 3
 								when obs regexp "!!2154=(6467|6964|792|633|631)!!" then @cur_arv_line := 1
 								when obs regexp "!!2154=(794|635|6160|6159)!!" then @cur_arv_line := 2
-								when obs regexp "!!2154=6156!!" then @cur_arv_line := 3
+								when obs regexp "!!2154=(6156|9759)!!" then @cur_arv_line := 3
 								when @prev_id = @cur_id then @cur_arv_line
 								else @cur_arv_line := null
 							end as cur_arv_line,
