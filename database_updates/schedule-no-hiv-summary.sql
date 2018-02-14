815bdf078c700360d8c3b6c21e77cf58e1062244
Nicky Kibor <nickykibor@Nickys-MacBook-Pro.local>
2018-02-14 10:03:51 +0300

    Added script to schedule patients with no hiv summary

---
+++ database_updates/schedule-no-hiv-summary.sql
@@ -0,0 +1,32 @@
+insert into etl.flat_hiv_summary_queue (SELECT
+    DISTINCT fo.person_id
+FROM
+    etl.flat_obs fo left outer join etl.flat_hiv_summary hs on fo.person_id = hs.person_id
+WHERE
+    fo.encounter_type IN (1 , 2,
+        3,
+        4,
+        10,
+        14,
+        15,
+        17,
+        19,
+        26,
+        32,
+        33,
+        34,
+        47,
+        105,
+        106,
+        112,
+        113,
+        114,
+        117,
+        120,
+        127,
+        128,
+        129,
+        138,
+        153,
+        154,
+        158) and hs.person_id is null limit 300);
